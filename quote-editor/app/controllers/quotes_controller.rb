40cacfa2637c2edfc09af9b3f072b016ce57639c
Viktoria Never <viktoria.never@clio.com>
2024-02-08 12:37:24 -0500

    reverted mistake Revert "issues, reverting commit"
    This reverts commit da1c0fa129a956c700a768f0b5261b7868e53616.

--- quote-editor/app/controllers/quotes_controller.rb
+++ quote-editor/app/controllers/quotes_controller.rb
@@ -14,20 +14,23 @@ class QuotesController < ApplicationController
   
     def create
       @quote = current_company.quotes.build(quote_params)
-  
+    
       if @quote.save
         respond_to do |format|
           format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
-          format.turbo_stream
+          format.turbo_stream { flash.now[:notice] = "Quote was successfully created." }
         end
       else
         render :new, status: :unprocessable_entity
       end
     end
   
+    def edit
+    end
+  
     def update
       if @quote.update(quote_params)
-        redirect_to quotes_path, notice: "Quote was successfully updated."
+        redirect_to quotes_path, notice: "Quote successfully updated."
       else
         render :edit, status: :unprocessable_entity
       end
@@ -35,7 +38,7 @@ class QuotesController < ApplicationController
   
     def destroy
       @quote.destroy
-  
+    
       respond_to do |format|
         format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
         format.turbo_stream

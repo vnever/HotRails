8d64a8e746c72411a33a5fbc304ee2a8aa7f3ad5
Viktoria Never <viktoria.never@clio.com>
2024-02-15 10:44:03 -0500

    line-tems

--- quote-editor/config/routes.rb
+++ quote-editor/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :quotes do
-    resources :line_item_dates, except: [:index, :show]
+    resources :line_item_dates, except: [:index, :show] do
+      resources :line_items, except: [:index, :show]
+    end
     resources :hash_tags, except: [:index, :show]
   end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
